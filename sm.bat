787ab48a2c1ddfa3e9741514c82433d718b66f50
horacio9a <horacio9a@gmail.com>
2018-02-22 03:37:11 +0100

    v.1.0.2 Batch script for py scripts start-up
    The problem of empty files has been resolved.
    You need the same directory you use in config.cfg in line 2 as 'output_folder' to edit line 5 of this batch script as 'OUT_DIR'.

--- sm.bat
+++ sm.bat
@@ -2,50 +2,48 @@
 SETLOCAL EnableDelayedExpansion
 :START
 CLS && ECHO.
-SET /P MODE=EXIT(6) LS-R(5) FF-R(4) RTMP-R(3) LS(2) FF(1) RTMP(0)(ENTER)(%MODE%): 
+SET OUT_DIR=C:\Videos\Streamate\
+SET /P MODE=EXIT(6) LS-R(5) FF-R(4) RTMP-R(3) LS(2) YT(1) RTMP(0)(ENTER)(%MODE%): 
 IF "%MODE%"=="" GOTO RTMP
 IF "%MODE%"=="0" GOTO RTMP
-IF "%MODE%"=="1" GOTO FF
+IF "%MODE%"=="1" GOTO YT
 IF "%MODE%"=="2" GOTO LS
 IF "%MODE%"=="3" GOTO RTMP-R
 IF "%MODE%"=="4" GOTO FF-R
 IF "%MODE%"=="5" GOTO LS-R
 IF "%MODE%"=="6" GOTO EXIT
 :RTMP
+COLOR 0F
 ECHO.
 CLS && ECHO ##################################################
 ECHO ### SM-RTMP #### R E C O R D I N G ###############
 ECHO ##################################################
 ECHO.
-cd/
-COLOR 0F
-cd -sm-py
+cd C:/-sm-py
 python sm.py
 ECHO.
 PAUSE
 GOTO START
-:FF
+:YT
+COLOR 0F
 ECHO.
 CLS && ECHO ##################################################
-ECHO ### SM-FF ###### R E C O R D I N G ###############
+ECHO ### SM-YT ###### R E C O R D I N G ###############
 ECHO ##################################################
 ECHO.
-COLOR 0F
-cd/
-cd -sm-py
-python smff.py
+cd C:/-sm-py
+python smyt.py
 ECHO.
 PAUSE
 GOTO START
 :LS
+COLOR 0F
 ECHO.
 CLS && ECHO ##################################################
 ECHO ### SM-LS ###### R E C O R D I N G ###############
 ECHO ##################################################
 ECHO.
-COLOR 0F
-cd/
-cd -sm-py
+cd C:/-sm-py
 python smls.py
 ECHO.
 PAUSE
@@ -68,6 +66,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :RTMP-R_
+COLOR 0F
 SET MODELNAME=%MODEL% ### %M% ##################################
 SET _MODEL_=%MODELNAME:~0,33%
 ECHO.
@@ -76,10 +75,11 @@ ECHO ### SM-RTMP-R ### R E C O R D I N G ###############
 ECHO ################# %_MODEL_%
 ECHO ###################################################
 ECHO.
-cd/
-COLOR 0F
-cd -sm-py
+cd C:/-sm-py
 python smr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
+cd C:/-sm-py
 TIMEOUT 30
 GOTO RTMP-R_
 :FF-R
@@ -100,6 +100,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :FF-R_
+COLOR 0F
 SET MODELNAME=%MODEL% ### %M% ##################################
 SET _MODEL_=%MODELNAME:~0,33%
 ECHO.
@@ -108,10 +109,11 @@ ECHO ### SM-FF-R ##### R E C O R D I N G ###############
 ECHO ################# %_MODEL_%
 ECHO ###################################################
 ECHO.
-COLOR 0F
-cd/
-cd -sm-py
+cd C:/-sm-py
 python smffr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
+cd C:/-sm-py
 TIMEOUT 30
 GOTO FF-R_
 :LS-R
@@ -132,6 +134,7 @@ SET /A n=n+1
 IF !n!==%M% SET MODEL=%%A
 )
 :LS-R_
+COLOR 0F
 SET MODELNAME=%MODEL% ### %M% ##################################
 SET _MODEL_=%MODELNAME:~0,33%
 ECHO.
@@ -140,10 +143,11 @@ ECHO ### SM-LS-R #### R E C O R D I N G ###############
 ECHO ################ %_MODEL_%
 ECHO ##################################################
 ECHO.
-COLOR 0F
-cd/
-cd -sm-py
+cd C:/-sm-py
 python smlsr.py %MODEL%
+cd %OUT_DIR%
+FOR /R %%F in (*) DO IF %%~ZF LSS 40 DEL "%%F"
+cd C:/-sm-py
 TIMEOUT 30
 GOTO LS-R_
 :EXIT
